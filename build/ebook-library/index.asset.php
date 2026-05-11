15026a0b264f1a8744c2aa985ece5875c28a92b3
Golam Kibria <soysob87@gmail.com>
2026-05-12 00:17:58 +0600

    Initial commit

---
+++ build/ebook-library/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0e25a4c574d216d5423f');
---
+++ build/ebook-library/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '565f65fb049643f6522c');
