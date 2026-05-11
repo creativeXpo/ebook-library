677d50953081fd50d53f79aae6e38adfd80253e0
Golam Kibria <soysob87@gmail.com>
2026-05-12 00:37:08 +0600

    Text Updated

--- src/ebook-library/edit.js
+++ src/ebook-library/edit.js
@@ -32,7 +32,7 @@ import './editor.scss';
 export default function Edit() {
 	return (
 		<p { ...useBlockProps() }>
-			{ __( 'Ebook Library – hello from the editor!', 'ebook-library' ) }
+			{ __( 'Hello World!', 'ebook-library' ) }
 		</p>
 	);
 }
